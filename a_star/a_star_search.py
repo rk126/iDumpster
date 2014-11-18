9d523b536bf770e42a0083090273dbffd413dc9d
rk126 <ramkrishnan.nk@gmail.com>
2014-11-18 18:55:20 -0500

    File/Dir structure definition v0.0

---
+++ a_star/a_star_search.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# Creates instances of the truck/dumpster/landfill selected and uses the 2-D map information and state information
+# to search an optimum path for a given set of truck-dumpster-landfill triplet
+# Takes care of the environmental objects like trees, other components, etc
