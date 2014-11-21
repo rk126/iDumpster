74cd61e313871a775483b530cf555400f36ef59a
bballtwenty8 <bballtwenty8@gmail.com>
2014-11-20 20:01:26 -0500

    Arg parse command line input and new get credentials function using reg ex

--- iDumpster.py
+++ iDumpster.py
@@ -3,7 +3,8 @@
 This file contains iDumpster.py which will read the sensor values over I2C and
 send them via RabbitMQ to the central server
 """
-
+import argparse
+import re
 import pika
 import pika.exceptions
 import signal
@@ -38,63 +39,73 @@ def read_Sensor():
     result = randint(1,9)
     return result
 
+def get_credentials(cmd_cred):
+    '''Translate a <username>:<password> string to a Pika credential'''
+    cred_re = r"(?P<un>.+)[:](?P<pw>.+)"
+
+    match = re.search(cred_re, cmd_cred)
+
+    if match:
+        return pika.PlainCredentials(match.group("un"),
+                                     match.group("pw"),
+                                     True)
+    else:
+        raise RuntimeError("Credentials Failed Regex Validation")
+
 # Application Entry Point
 # ^^^^^^^^^^^^^^^^^^^^^^^
 
 # Guard try clause to catch any errors that aren't expected
 try:
-    #Set each of the default values
-    # The message broker host name or IP address
-    host = None
-    # The virtual host to connect to
-    vhost = "/" # Defaults to the root virtual host
-    # The credentials to use
-    credentials = None
-    # The topic to subscribe to
-    topic = None
-    # Capacity of the dumpster (must be greater than zero) units: yards^3
-    capacity = 0
-    # Location of the dumpster (must be entered as "x,y" coordinate
-    location = [-1,-1]
+    # Create the argument parser
+    parser = argparse.ArgumentParser()
 
-    #parse through command line arguments and assign parameters
-    #if nothing is passed, defaults as set above are used
-    if sys.argv:
-        for i in range(0,len(sys.argv)):
-            if sys.argv[i] == "-b":
-                host = sys.argv[i+1]
-            elif sys.argv[i] == "-p":
-                vhost = sys.argv[i+1]
-            elif sys.argv[i] == "-c":
-                credentials = sys.argv[i+1]
-		credentials = credentials.split(':')
-		username = credentials[0]
-		password = credentials[1]
-		credentials = pika.PlainCredentials(username, password)
-            elif sys.argv[i] == "-k":
-                topic = sys.argv[i+1]
-            elif sys.argv[i] == "-v":
-                capacity = sys.argv[i+1]
-            elif sys.argv[i] == "-m":
-                location = sys.argv[i+1]
-                location = location.split(',')
+    # Add All Desired Arguments to the Parser
+    parser.add_argument("-b", "--broker_address",
+            help="This is the ip address or domain of the message broker",
+            metavar="message broker", default="omaha.local", type=str,
+            required=True)
+    parser.add_argument("-p", "--path",
+            help="Path to Virtual Host to connect to on the message broker.",
+            metavar="virtual host", default="/", type=str)
+    parser.add_argument("-c", "--credentials",
+            help="Credentials to use when logging in to message broker",
+            metavar="login:password", default="guest:guest", type=str)
+    parser.add_argument("-k", "--key",
+            help="The routing key for publishing messages to the message broker",
+            metavar="routing key", default="iDumpster", type=str, required=True)
+    parser.add_argument("-v", "--capacity",
+            help="Volume that this trash can will hold before becoming full",
+            metavar="volume", default=0, type=int, required=True)
+    parser.add_argument("-x", "--locx",
+            help="Fake location of the dumpster on x axis", metavar="x",
+            default="0", type=int, required=True)
+    parser.add_argument("-y", "--locy",
+            help="Fake location of the dumpster on y axis", metavar="y",
+            default="0", type=int, required=True)
+
+    # Parse arguments
+    args = parser.parse_args()
+
+    # Store Arguments
+    host = args.broker_address
+    vhost = args.path
+    # This will throw a runtime exception with improper formatted cli
+    credentials = get_credentials(args.credentials)
+    topic = args.key
+    capacity = args.capacity
+    location = [args.locx, args.locy]
 
     # Ensure that the user specified the required arguments
-    if host is None:
-        print "You must specify a message broker to connect to"
-        sys.exit()
-
-    if topic is None:
-        print "You must specify a topic to subscribe to"
-        sys.exit()
-
-    if capacity == 0:
-        print "You must specify the dumpster capacity"
-        sys.exit()
     if location[0] < 0 or location[1] < 0:
         print "You must specify a valid x,y coordinate for the dumpster"
         sys.exit()
-        
+
+    # Ensure there is dumpster capacity
+    if not capacity > 0:
+        print "The dumpster must have > 0 volume"
+        sys.exit()
+
     # Setup signal handlers to shutdown this app when SIGINT or SIGTERM is
     # sent to this app
     signal_num = signal.SIGINT
@@ -105,14 +116,14 @@ try:
 
     except ValueError, ve:
         print "Warning: Graceful shutdown may not be possible: Unsupported " \
-                "Signal: " + signal_num    
+                "Signal: " + signal_num
 
     try:
         # Connect to the message broker using the given broker address (host)
         # Use the virtual host (vhost) and credential information (credentials),
         # if provided
         message_broker = pika.BlockingConnection(pika.ConnectionParameters(
-        host,virtual_host=vhost,credentials=credentials))
+                host,virtual_host=vhost,credentials=credentials))
         # Setup the channel and exchange
         channel = message_broker.channel()
         channel.exchange_declare(exchange='dumpster_data',type='direct')
@@ -127,14 +138,14 @@ try:
             level.insert(0,read_Sensor())
             time.sleep(2)
         print level
-            
+
         # Loop until the application is asked to quit
         while(publish_levels):
             # Read level
             level.insert(0,read_Sensor()) #inserts new reading at start of list
             level.pop() #removes last item in list
             print level
-            
+
             #compute average of previous 5 values
             x = float(sum(level))
             y = float(len(level))
@@ -143,7 +154,7 @@ try:
             #   Publish the message (utilization_msg) in JSON format to the
             #   broker under the user specified topic.
             data = json.dumps(dumpster_data,indent=4,sort_keys=True)#put dict into JSON format
-            
+
             channel.basic_publish(exchange = 'dumpster_data', routing_key = topic,
                                   body = data)
             print "Sent: ", data
@@ -158,14 +169,14 @@ try:
     except pika.exceptions.ProbableAuthenticationError, aue:
         print >> sys.stderr, "Error: A Probable Authentication error occured: " + str(aue.message)
         print "Please enter a valid username/password in the format '-c username:password'"
-        
+
     except pika.exceptions.AMQPConnectionError, acoe:
         print >> sys.stderr, "Error: An AMQP Connection Error occured: " + str(acoe.message)
         print "Please enter a valid RabbitMQ host name in the format '-b WabbitHost'"
 
     except pika.exceptions.AMQPChannelError, ache:
         print >> sys.stderr, "Error: An AMQP Channel Error occured: " + str(ache.message)
-    
+
     except pika.exceptions.ChannelError, ce:
         print >> sys.stderr, "Error: A channel error occured: " + str(ce.message)
     except pika.exceptions.AMQPError, ae:
@@ -173,7 +184,7 @@ try:
     #General catch-all handler as last resort
     except Exception, eee:
         print >> sys.stderr, "Error: An unexpected exception occured: " + str(eee.message)
-    
+
     finally:
         # Attempt to gracefully shutdown the connection to the message broker
         print "Application Shutting Down..."
